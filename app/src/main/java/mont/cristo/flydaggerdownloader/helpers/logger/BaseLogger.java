187f973fd8796d2f2e2e3885389ba674d0b0abb1
bongdem master <bongdem master>
2016-01-06 22:42:19 +0700

    Implement show more method from call stack when show log

--- app/src/main/java/mont/cristo/flydaggerdownloader/helpers/logger/BaseLogger.java
+++ app/src/main/java/mont/cristo/flydaggerdownloader/helpers/logger/BaseLogger.java
@@ -1,10 +1,7 @@
 package mont.cristo.flydaggerdownloader.helpers.logger;
 
 public abstract class BaseLogger implements Loggable {
-
-    private StackTraceElement getCurrentStackTrace() {
-        return Thread.currentThread().getStackTrace()[3];
-    }
+    private static final int INDEX_STACK_TRACE_DEFAULT = 3;
 
     /**
      * This method to get current log tag to find the current class run this log method
@@ -12,7 +9,7 @@ public abstract class BaseLogger implements Loggable {
      * @return Log tag of current java class
      */
     protected String getLogTag() {
-        String fileName = getCurrentStackTrace().getFileName();
+        String fileName = Thread.currentThread().getStackTrace()[INDEX_STACK_TRACE_DEFAULT].getFileName();
         return fileName.substring(0, fileName.lastIndexOf(".") + 1);
     }
 
@@ -21,8 +18,18 @@ public abstract class BaseLogger implements Loggable {
      *
      * @return Caller method name
      */
-    protected String getMethodName() {
-        return getCurrentStackTrace().getMethodName();
+    protected String getMethodName(int countMethod) {
+        StackTraceElement[] stackTraces = Thread.currentThread().getStackTrace();
+        // File the maximum method can shown
+        int maxIndexShownMethod = countMethod + INDEX_STACK_TRACE_DEFAULT;
+        if (stackTraces.length <= maxIndexShownMethod) {
+            maxIndexShownMethod = stackTraces.length - 1;
+        }
+        String methodName = "";
+        for (int i = maxIndexShownMethod; i >= INDEX_STACK_TRACE_DEFAULT; i--) {
+            methodName += stackTraces[i].getMethodName() + " >>> ";
+        }
+        return methodName;
     }
 
     /**
@@ -51,7 +58,16 @@ public abstract class BaseLogger implements Loggable {
      * @return The optimized message
      */
     protected String getOptimizedMessage(String message) {
-        return "Process-" + getProcessId() + " >>> Thread-" + getThreadId() + " >>> " + getMethodName() + " >>> " + message;
+        return getOptimizedMessage(message, 0);
+    }
+
+    /**
+     * The same as getOptimizedMessage but can use numberMethod optional to show more call method order from call stack
+     *
+     * @param numberMethod Number method need to show more
+     */
+    protected String getOptimizedMessage(String message, int numberMethod) {
+        return "Process-" + getProcessId() + " >>> Thread-" + getThreadId() + " >>> " + getMethodName(numberMethod) + message;
     }
 
     /**
@@ -63,7 +79,16 @@ public abstract class BaseLogger implements Loggable {
      * @return The optimized message
      */
     protected String getOptimizedWarning(String message, Exception e) {
-        return getOptimizedMessage(message) + " >>> " + "Exception: " + e;
+        return getOptimizedWarning(message, e, 0);
+    }
+
+    /**
+     * The same as getOptimizedWarning but can use numberMethod optional to show more call method order from call stack
+     *
+     * @param numberMethod Number method need to show more
+     */
+    protected String getOptimizedWarning(String message, Exception e, int numberMethod) {
+        return getOptimizedMessage(message, numberMethod) + " >>> " + "Exception: " + e;
     }
 
 }
